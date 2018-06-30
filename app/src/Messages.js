b71ff1ecb128faea9a6b625c2f72cb8f9938bec5
Leonardo Alves <alexacaalves@gmail.com>
2018-06-30 19:24:57 -0300

    add Message component

---
+++ app/src/Messages.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import {
+    Button, 
+    Card,
+    Icon,
+    TextField,
+    Grid,
+} from '@material-ui/core';
+
+const styles = theme =>  ({
+    dcIcon: {
+        marginRight: theme.spacing.unit,
+    }
+});
+
+class Messages extends Component {
+    state = {
+        newMsg: '',
+        messages: [],
+    }
+
+    handleChange = name => event => {
+        this.setState({
+          [name]: event.target.value,
+        });
+    };
+
+    sendMessage = (event) => {
+        if (event.key !== 'Enter') return;
+
+        this.props.send(this.state.newMsg);
+        this.pushMessage(this.state.newMsg, this.props.nickname());
+        this.setState({ newMsg: '' });
+    }
+
+    pushMessage = (msg, userName) => {
+        const m = `[${new Date().toLocaleString()}] ${userName}: ${msg}`
+        this.state.messages.push(m);
+        this.setState({ messages: this.state.messages });
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { messages } = this.state;
+        return (
+            <div>
+
+                <Card> 
+                    <Button style={{ float: 'right' }}>
+                        <Icon className={classes.dcIcon}>call_end</Icon>
+                        Disconnect
+                    </Button>
+                    <p>Connected to: localhost</p>
+                    <Grid container justify="center" direction="column">
+                        <Grid item xs>
+                            { messages.map((o, i) => <p style={{ textAlign: 'left' }} key={i}>{o}</p>)}
+                        </Grid>
+                        <Grid item xs>
+                            <TextField
+                                style={{ width: '95%', marginBottom: '10px' }}
+                                label="Write a message"
+                                className={classes.textField}
+                                value={this.state.newMsg}
+                                onChange={this.handleChange('newMsg')}
+                                onKeyDown={this.sendMessage.bind(this)}>
+                            </TextField>
+                        </Grid>
+                        
+                        <Grid item xs/>
+                    </Grid>
+                    
+                </Card> 
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Messages);
