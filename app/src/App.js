30b4e07b7dac18b0913180d72dafab023c85d41f
Leonardo Alves <alexacaalves@gmail.com>
2018-06-30 19:26:04 -0300

    Handle view after ws connect

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import  Greeting from './Greeting';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Snackbar from '@material-ui/core/Snackbar';
+import Messages from './Messages';
 
 import { fromEvent } from 'rxjs';
 
@@ -18,17 +19,21 @@ const styles = {
 
 class App extends Component {
   state = {
+    nickname: '',
     loading: false,
     snackBar: {
       open: false,
       msg: ''
     },
+    connection: null,
+    connected: false,
   };
 
-  connectToServer = (url) => {
-    this.setState({ loading: true });
+  connectToServer = (url, nickname) => {
+    if (!nickname || !url) return;
+
+    this.setState({ loading: true, nickname: nickname });
     const connection = new WebSocket(url);
-    connection.addEventListener('open', () => { console.log('opened')})
     const open = fromEvent(connection, 'open');
     const message = fromEvent(connection, 'message');
     const error = fromEvent(connection, 'error');
@@ -40,27 +45,37 @@ class App extends Component {
       });
     });
 
-    message.subscribe(e => {
-      this.setState({ 
-        snackBar: { open: true, msg: 'Connection success!' },
-        loading: false,
-      })
-    });
-
     open.subscribe(e => {
       this.setState({ 
         snackBar: { open: true, msg: 'Connection success!' },
         loading: false,
-      })
+        connected: true,
+        connection: connection,
+      });
+
+      connection.send(`[HI]${this.state.nickname}[|HI]\n`);
     });
+
+    message.subscribe(e => {
+      console.log(e);
+    })
+  }
+
+  sendMessage = msg => {
+    this.state.connection.send(msg);
   }
 
   closeSnackBar = () => {
     this.setState({ snackBar: { open: false, msg: '' } })
   }
 
+  getNickname () {
+    return this.state.nickname;
+  }
+
   render() {
-    const { loading } = this.state;
+    const { loading, connected } = this.state;
+
     return (
       <div className="App">
         <Snackbar
@@ -81,12 +96,14 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        {loading !== false && <LinearProgress color="secondary"/>}
+        { loading !== false && <LinearProgress color="secondary"/> }
 
-        <Greeting connect={this.connectToServer.bind(this)}/>
+        { connected !== true && <Greeting connect={this.connectToServer}/> }
+
+        { connected === true && <Messages nickname={this.getNickname.bind(this)} send={this.sendMessage.bind(this)} style={{ height:'100%' }}/> }
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
